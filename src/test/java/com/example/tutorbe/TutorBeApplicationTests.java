6a01b2073d5c3660ee0481176e8dbb2c73c61a48
MusienkoYuriy97 <97musienko@gmail.com>
2022-12-27 12:01:16 +0100

    T-001: Initiate a project

---
+++ src/test/java/com/example/tutorbe/TutorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
